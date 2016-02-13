d11125cfd6a1b4f6a8d1ec3291eaf79a52c2978e
Sam Redmond <sredmond@stanford.edu>
2016-02-12 17:11:35 -0800

    updating src files

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,19 +1,27 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+/* File: background.js
+ * -------------------
+ * This script defines an event page, which allows you
+ * to have a "single long-running script to handle some
+ * task or state."
+ *
+ * The general principle is that this script will register
+ * to listen for Chrome events, and then be instantiated
+ * when Chrome notices that event.
+ *
+ * (You don't need to change this file unless you're building
+ * an extension.)
+ * 
+ * Note: Since the event page is created and destroyed many
+ * times throughout a browser session, you cannot store
+ * instance state in global variables.
+ * 
+ * An example chrome extension that uses an event page is
+ * available at:
+ * https://developer.chrome.com/extensions/samples#search:sandbox
+ * 
+ * For more details, check out the documentation:
+ * https://developer.chrome.com/extensions/event_pages
+ * 
+ */
 
-var settings = new Store("settings", {
-    "sample_setting": "This is how you use Store.js to remember values"
-});
-
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
-// chrome.storage.local.set({'key':'value'}, function() {alert('Set message');});
-
-// chrome.storage.local.get('key', function(value) {
-// 	console.log(value)
-// });
+/* EMPTY */
