619b9ba623bd43cdb9893d12409f69ed0ba5cb06
Sam Redmond <sredmond@stanford.edu>
2016-02-12 17:15:40 -0800

    Adds explanations to browser_action javascript

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,3 +1,12 @@
+/* File: browser_action.js
+ * -----------------------
+ * This javascript file listens for a click on the submit
+ * button, and responds by saving the current value of the
+ * textarea to Chrome's local storage. You shouldn't have to
+ * change this file unless you also change the corresponding
+ * browser_action.html file.
+ */
+
 window.onload = function() {
 	// Executes when HTML document is loaded and DOM is ready
 	var $submit = document.getElementById('submit');
@@ -6,8 +15,9 @@ window.onload = function() {
 		var $textarea = document.getElementById('settings');
 		var text = $textarea.value;
 	    chrome.storage.local.set({'settings': text}, function() {
-	    	// This code block is executed once the key/value pair is saved to local storage
-	    	// console.log("Successfully saved data")
+	    	// This code block is executed asynchronously, once
+	    	// the key/value pair is saved to local storage
+	    	console.log("Data Saved");
 	    });
 	});
 };
