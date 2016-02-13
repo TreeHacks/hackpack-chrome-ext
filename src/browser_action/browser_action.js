b2a708a85009eb95e1bb272d4be14882fa42e43b
Sam Redmond <sredmond@stanford.edu>
2016-02-12 18:41:19 -0800

    Rewrite browser action logic for clarity

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -7,17 +7,46 @@
  * browser_action.html file.
  */
 
-window.onload = function() {
-	// Executes when HTML document is loaded and DOM is ready
-	var $submit = document.getElementById('submit');
-
-	$submit.addEventListener('click', function() {
-		var $textarea = document.getElementById('settings');
-		var text = $textarea.value;
-	    chrome.storage.local.set({'settings': text}, function() {
-	    	// This code block is executed asynchronously, once
-	    	// the key/value pair is saved to local storage
-	    	console.log("Data Saved");
-	    });
+// Saves settings to chrome.storage.local.
+function save_settings() {
+	var $textarea = document.getElementById('settings');
+	var text = $textarea.value;
+	chrome.storage.local.set({
+		'settings': text
+	}, function() {
+		// This code block is executed when the settings are saved.
+		var $status = document.getElementById('status');
+		$status.textContent = "Settings saved.";
+		setTimeout(function() {
+		  status.textContent = '';
+		}, 1000);
 	});
-};
+}
+
+// Retrieves settings from chrome.storage.local.
+function restore_settings() {
+	chrome.storage.local.get('settings', function(response) {
+		var text = response.settings || "";
+		document.getElementById('settings').value = text;
+	});
+}
+
+// Clear all settings in chrome.storage.local
+function clear_settings() {
+	chrome.storage.local.clear(function() {
+		var $textarea = document.getElementById('settings');
+		$textarea.value = "";
+		var $status = document.getElementById('status');
+		$status.textContent = "Settings cleared.";
+		setTimeout(function() {
+		  status.textContent = '';
+		}, 1000);
+	});
+}
+
+// Restore settings when the DOM loads
+document.addEventListener('DOMContentLoaded', restore_settings);
+// Save settings when the save button is clicked.
+document.getElementById('save').addEventListener('click', save_settings);
+// Clear settings when the clear button is clicked.
+document.getElementById('clear').addEventListener('click', clear_settings);
